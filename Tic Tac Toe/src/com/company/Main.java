200ef7f90a5eb21c4f358e76c29aec4e956ce2af
Kevin-He-DVHS <32375681+Kevin-He-DVHS@users.noreply.github.com>
2017-09-28 18:20:18 -0700

    Add files via upload
    Fix issues like infinite loop and number input with whitespace

--- Tic Tac Toe/src/com/company/Main.java
+++ Tic Tac Toe/src/com/company/Main.java
@@ -11,7 +11,7 @@ public class Main {
     static String turn;
 
     public static void main(String[] args) {
-        input = new Scanner(System.in);
+        input = new Scanner(System.in).useDelimiter("[\\n\\r\\f]+");
         board = new String[9];
         turn = "X";
         String winner = null;
@@ -19,16 +19,9 @@ public class Main {
 
 
         printBoard();
-        System.out.println("IMPORTANT RULE:");
-        System.out.println("When entering a slot number, DO NOT enter in a SPECIAL CHARACTERS or LETTERS! It will result in an AUTOMATIC LOSS!");
-        System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------");
         System.out.println("X's will play first. Enter a slot number to place X in:");
 
         while (winner == null) {
-            while(!input.hasNextInt()){
-                input.next();
-                System.out.println("Invalid input; re-enter slot number:");
-            }
             int numInput;
             try {
                 numInput = input.nextInt();
@@ -37,8 +30,9 @@ public class Main {
                     continue;
                 }
             } catch (InputMismatchException e) {
-                System.out.println("Invalid input! " + turn + "'s have lost.");
-                break;
+                input.next();
+                System.out.println("Invalid input; re-enter slot number:");
+                continue;
             }
             if (board[numInput-1].equals(String.valueOf(numInput))) {
                 board[numInput-1] = turn;
@@ -122,4 +116,5 @@ public class Main {
         for (int a = 0; a < 9; a++) {
             board[a] = String.valueOf(a+1);
         }
+    }
 }
